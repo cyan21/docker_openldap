1e4d533c9bd1fd22a3cb936b452cd8540912c230
Yann Chaysinh <yann.chaysinh@gmail.com>
2017-08-01 15:57:23 +0000

    add init.sh for ubuntu

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
-ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
-ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
-
 ldapadd -Y EXTERNAL -H ldapi:/// -f /tmp/memberof.ldif
 ldapadd -D  cn=Manager,dc=jfrog,dc=com -w jfrog -f /tmp/root.ldif
 ldapadd -D  cn=Manager,dc=jfrog,dc=com -w jfrog -f /tmp/users.ldif
